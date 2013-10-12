ab8ec2d2912ff82df40df5f87ddd4237753c09da
Anthony Candaele <anthony.candaele@webmove.be>
2013-10-12 11:36:49 +0200

    add UiController

---
+++ app/controllers/ui_controller_controller.rb
@@ -0,0 +1,11 @@
+class UiControllerController < ApplicationController
+  before_filter do
+    redirect_to :root if Rails.env.production?
+  end
+
+  layout "application"
+
+  def index
+
+  end
+end
