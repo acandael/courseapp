25db2f263e8faa317ac47e9edaecaf6988feea35
Anthony Candaele <anthony.candaele@webmove.be>
2013-10-12 10:45:18 +0200

    configure app to use postgres

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Courseapp::Application.config.secret_key_base = 'a2acfe77b94b1260b5cf2bc9810e615b3f57c2caadd981305e784a9a99c6524056e04d24f44ab3cef0d22c461f24e997ce3a802ae0aed32a2afbce9b482f8256'
