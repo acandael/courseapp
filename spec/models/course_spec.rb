6fd5e7e7dc7685a9525ba1d47574567d212cf934
Anthony Candaele <anthony.candaele@webmove.be>
2013-10-16 21:12:52 +0200

    first test passing

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -4,7 +4,7 @@ describe Course do
   it "saves itself" do
     course = Course.new(title: "course1", description: "this is the description for course 1")
     course.save
-    expect(course.first).to eq(course)
+    expect(Course.first).to eq(course)
   end
   
 end
