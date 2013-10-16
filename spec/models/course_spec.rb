4893fab0c328e141e68b98e90507fc866cb0b724
Anthony Candaele <anthony.candaele@webmove.be>
2013-10-16 22:09:46 +0200

    write test for has_many :modules association

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -1,10 +1,12 @@
 require 'spec_helper'
 
 describe Course do
+  it { should have_many(:modules) }
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:description) }
   it "saves itself" do
     course = Course.new(title: "course1", description: "this is the description for course 1")
     course.save
     expect(Course.first).to eq(course)
   end
-  
 end
