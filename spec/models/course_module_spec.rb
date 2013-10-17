a2faab9da9a9f38cd8188b901439d04ee37321d1
Anthony Candaele <anthony.candaele@webmove.be>
2013-10-17 18:24:28 +0200

    add stuff

--- spec/models/course_module_spec.rb
+++ spec/models/course_module_spec.rb
@@ -9,4 +9,10 @@ describe CourseModule do
     course_module.save
     expect(CourseModule.first). to eq(course_module)
   end
+
+  it "belongs to course" do
+    course = Course.create(title: "course1", description: "description for course 1") 
+    module1 = CourseModule.create(title: "module 1", description: "description for module 1", course_id: course.id)
+    expect(course.course_modules.first).to eq(module1)
+  end
 end
