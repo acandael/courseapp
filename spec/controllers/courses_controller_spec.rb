29cb75024a468a4cd7a8182aeb5684334b041d0b
Anthony Candaele <anthony.candaele@webmove.be>
2013-10-19 10:07:12 +0200

    install fabrication and faker gem

---
+++ spec/controllers/courses_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe CoursesController do
+  describe "GET #show" do
+    it "set @course" do
+      course = Fabricate(:course)
+      get :show, id: course.id
+      expect(assigns(:course)).to eq(course)
+    end
+end
