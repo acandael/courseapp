a8ddb14ea2e923e7271f867b0f35cbde03f7e59b
Anthony Candaele <anthony.candaele@webmove.be>
2013-10-19 09:53:07 +0200

    implementing test for course model

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe Course do
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:description) }
   it "saves itself" do
     course = Course.new(title: "course1", description: "this is the description for course 1")
     course.save
