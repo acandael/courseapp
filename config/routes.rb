d7496507872460ee772b5d2e9d15b532fe4957e8
Anthony Candaele <anthony.candaele@webmove.be>
2013-10-23 19:40:23 +0200

    creat front page and register page

--- config/routes.rb
+++ config/routes.rb
@@ -53,7 +53,10 @@ Courseapp::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  root to: "pages#front"
   get 'ui(/:action)', controller: 'ui'
+  get 'pages/subscription_plans', controller: 'pages'
+  get 'register', to: "users#new"
   get 'courses/:id/chapters/:chapter_id/video/:video_id', controller: 'chapters', action: 'show', as: 'show_video'
  
   get 'quizzes/:id/question/:question_id', controller: 'quizzes', action: 'show', as: 'show_question'
