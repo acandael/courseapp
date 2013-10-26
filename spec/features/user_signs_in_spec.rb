eaa84411b3cd55bd986c0e34d74b312adb804e9c
Anthony Candaele <anthony.candaele@webmove.be>
2013-10-26 13:33:51 +0200

    implement user interacts with course integration test

--- spec/features/user_signs_in_spec.rb
+++ spec/features/user_signs_in_spec.rb
@@ -6,10 +6,7 @@ feature 'User signs in' do
   end
   scenario "with valid email and password" do
     alice = Fabricate(:user)
-    visit sign_in_path
-    fill_in "Email Address", with: alice.email
-    fill_in "Password", with: alice.password
-    click_button "Sign In"
+    sign_in(alice)
     page.should have_content alice.full_name
   end
 
