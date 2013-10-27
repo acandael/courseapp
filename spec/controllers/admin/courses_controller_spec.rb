558fa40abd2e7957debb4f02951b7214af796cd6
Anthony Candaele <anthony.candaele@webmove.be>
2013-10-27 10:46:48 +0100

    implemnt add a course functionality

--- spec/controllers/admin/courses_controller_spec.rb
+++ spec/controllers/admin/courses_controller_spec.rb
@@ -5,21 +5,69 @@ describe Admin::CoursesController do
     it_behaves_like "requires sign in" do
       let(:action) { get :new }
     end
+    it_behaves_like "requires admin" do
+      let(:action) { get :new }
+    end
     it "sets the course to a new course" do
       set_current_admin
       get :new
       expect(assigns(:course)).to be_instance_of(Course)
       expect(assigns(:course)).to be_new_record
     end
-    it "redirects the regular user to the home path" do
-      set_current_user
-      get :new
-      expect(response).to redirect_to home_path
-    end
     it "sets the flash error message for regular user" do
       set_current_user
       get :new
       expect(flash[:error]).to be_present
+    end
+  end
+
+  describe "POST #create" do
+    it_behaves_like "requires sign in" do
+      let(:action) { post :create }
+    end
+    
+    it_behaves_like "requires admin" do
+      let(:action) { post :create }
+    end
+
+    context "with valid input" do
+      it "redirects to the courses index page" do
+        set_current_admin
+        post :create, course: { title: "the safe workplace", description: "what you should know about a safe workplace" }
+        expect(response).to redirect_to admin_courses_path
+      end
+      it "creates a new course" do
+        set_current_admin
+        post :create, course: { title: "the safe workplace", description: "what you should know about a safe workplace" }
+        expect(Course.count).to eq(1)
+      end
+      it "sets the flash success message" do
+        set_current_admin
+        post :create, course: { title: "the safe workplace", description: "what you should know about a safe workplace" }
+        expect(flash[:success]).to be_present 
+      end
+    end
+    context "with invalid input" do
+      it "does not create a new course" do
+        set_current_admin
+        post :create, course: { description: "what you should know about a safe workplace" }
+        expect(Course.count).to eq(0) 
+      end
+      it "renders the :new template" do
+        set_current_admin
+        post :create, course: { description: "what you should know about a safe workplace" }
+        expect(response).to render_template :new 
+      end
+      it "sets the @course variable" do
+        set_current_admin
+        post :create, course: { description: "what you should know about a safe workplace" }
+        expect(assigns(:course)).to be_present 
+      end
+      it "set the flash error message" do
+        set_current_admin
+        post :create, course: { description: "what you should know about a safe workplace" }
+        expect(flash[:error]).to be_present 
+      end
     end
   end
 end
