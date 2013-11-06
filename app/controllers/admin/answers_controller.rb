202767a59f55584cc891a9492607d7e6e9a53124
Anthony Candaele <anthony.candaele@webmove.be>
2013-11-06 16:33:31 +0100

    implement create answer

--- app/controllers/admin/answers_controller.rb
+++ app/controllers/admin/answers_controller.rb
@@ -6,8 +6,21 @@ class Admin::AnswersController < ApplicationController
     @answer = Answer.new
     @question = Question.find(params[:question_id])
     @quiz = Quiz.find(@question.quiz_id)
-    @chapter = Chapter.find(@quiz.chapter_id)
     @answer.question_id = @question.id
+  end
+
+  def create
+    @answer = Answer.create(answer_params(params[:answer]))
+    @answer.question_id = params[:question_id]
+    question = Question.find(@answer.question_id)
+    quiz = Quiz.find(question.quiz_id)
+    if @answer.save
+      flash[:success] = "You successfully added the answer: '#{@answer.title}'."
+      redirect_to admin_quiz_question_path(quiz.id, question.id)
+    else
+      flash[:error] = "The answer is not created. Please check the error messages."
+      render :new
+    end
   end
 
   private
@@ -18,5 +31,9 @@ class Admin::AnswersController < ApplicationController
       flash[:error] = "You are not authorized to do that."
       redirect_to home_path
     end
+  end
+
+  def answer_params(param)
+    params.require(:answer).permit(:title, :is_correct, :question_id)
   end
 end
--- spec/controllers/admin/answers_controller_spec.rb
+++ spec/controllers/admin/answers_controller_spec.rb
@@ -1,9 +1,20 @@
 require 'spec_helper'
 
 describe Admin::AnswersController do
+  describe "GET #show" do
+    before do
+      @quiz = Fabricate(:quiz)
+      @question = Fabricate(:question, quiz_id: @quiz.id)
+      @answer = Fabricate(:answer, question_id: @question.id)
+    end
+    it_behaves_like "requires sign in" do
+      let(:action) { get :show, question_id: @question.id, id: @answer.id }
+    end
+  end
   describe "GET #new" do
     before do
-      @question = Fabricate(:question)
+      @quiz = Fabricate(:quiz)
+      @question = Fabricate(:question, quiz_id: @quiz.id)
       @answer = Fabricate(:answer, question_id: @question.id)
     end
     it_behaves_like "requires sign in" do
@@ -28,6 +39,65 @@ describe Admin::AnswersController do
       set_current_user
       get :new, question_id: @question.id
       expect(flash[:error]).to be_present
+    end
+  end
+
+  describe "POST #create" do
+    before do
+      @quiz = Fabricate(:quiz)
+      @question = Fabricate(:question, quiz_id: @quiz.id)
+    end
+    it_behaves_like "requires sign in" do
+      let(:action) { post :create, question_id: @question.id }
+    end
+    it_behaves_like "requires admin" do
+      let(:action) { post :create, question_id: @question.id }
+    end
+
+    context "with valid input" do
+      it "redirects to the question show page" do
+        set_current_admin
+        post :create, question_id: @question.id, answer: { title: "David Heinemeier Hanson", is_correct: true }
+        expect(response).to redirect_to admin_question_path(@question.id)
+      end
+      it "creates a new answer" do
+        set_current_admin
+        post :create, question_id: @question.id, answer: { title: "David Heinemeier Hanson", is_correct: true }
+        expect(Answer.count).to eq(1)
+      end
+      it "sets the flash success message" do
+        set_current_admin
+        post :create, question_id: @question.id, answer: { title: "David Heinemeier Hanson", is_correct: true }
+        expect(flash[:success]).to be_present
+      end
+      it "set the property is_correct" do
+        set_current_admin
+        post :create, question_id: @question.id, answer: { title: "David Heinemeier Hanson", is_correct: true }
+        expect(assigns(:answer).is_correct?).to be_true
+      end
+    end
+    context "with invalid input" do
+      it "renders the :new template" do
+        set_current_admin
+        post :create, question_id: @question.id, answer: { title: "", is_correct: true }
+        expect(response).to render_template :new
+      end
+      it "does not create a new answer" do
+        set_current_admin
+        post :create, question_id: @question.id, answer: { title: "", is_correct: true }
+        expect(Answer.count).to eq(0)
+      end
+      it "sets the @answer variable" do
+        set_current_admin
+        post :create, question_id: @question.id, answer: { title: "", is_correct: true} 
+        expect(assigns(:answer)).to be_present
+
+      end
+      it "sets the flash error message" do
+        set_current_admin
+        post :create, question_id: @question.id, answer: { title: "", is_correct: true} 
+        expect(flash[:error]).to be_present
+      end
     end
   end
 end
