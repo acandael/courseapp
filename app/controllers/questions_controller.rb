ba808d339963d168358ff99db59e25ce653ed6d1
Anthony Candaele <anthony.candaele@webmove.be>
2013-11-13 20:49:59 +0100

    successfully implement user takes quiz scenario, woohoo

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,6 +2,10 @@ class QuestionsController < ApplicationController
   def update
     @quiz = Quiz.find(params[:quiz_id])
     @question = Question.subsequent_question(params[:id])
-    redirect_to quiz_path(@quiz.id, question_id: @question.id)
+    if @question != nil
+      redirect_to quiz_path(@quiz.id, question_id: @question.id)
+    else
+      redirect_to quiz_complete_path(@quiz.id)
+    end
   end
 end
