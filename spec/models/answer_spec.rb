e752e67f3d53179f0be2b366a2919bb879f36348
Anthony Candaele <anthony.candaele@webmove.be>
2013-11-24 12:37:33 +0100

    renamed correct to correct? for readability

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -18,20 +18,20 @@ describe Answer do
   it "returns true when answer is correct" do
     answer = Fabricate(:answer)
     answer.correct = true
-    expect(answer.correct).to be_true
+    expect(answer.correct?).to be_true
   end
 
   it "returns false when answer is not correct" do
     answer = Fabricate(:answer)
     answer.correct = false
-    expect(answer.correct).to be_false
+    expect(answer.correct?).to be_false
   end
 
   it "sets is_correct to true when there is no other correct answer" do
     question = Fabricate(:question)
     answer1 = Fabricate(:answer, correct: false, question_id: question.id)
     answer2 = Fabricate(:answer, correct: true, question_id: question.id)
-    expect(answer2.correct).to be_true 
+    expect(answer2.correct?).to be_true 
   end
   
 end
