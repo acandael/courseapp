84147529a8c73b4d497ab983b0c7a4c9503a2e06
Anthony Candaele <anthony.candaele@webmove.be>
2013-12-05 19:55:24 +0100

    fix badge_image on chapters list

--- app/uploaders/badge_image_uploader.rb
+++ app/uploaders/badge_image_uploader.rb
@@ -1,5 +1,11 @@
 class BadgeImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
-  process :resize_to_fill => [220, 220]
+  version :thumb do
+    process :resize_to_fill => [220, 220]
+  end
+
+  version :small_thumb, :from_version => :thumb do
+    process :resize_to_fill => [20, 20]
+  end
 end
